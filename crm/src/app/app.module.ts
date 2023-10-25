bdf62a73c1e4caac45a14d5edf8c53e9ee69b265
Nikoquattro <nikoquattro@gmail.com>
2023-10-25 22:30:23 +0200

    import des modules

--- crm/src/app/app.module.ts
+++ crm/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { SharedModule } from './shared/shared.module';
+import { UiModule } from './ui/ui.module';
+import { ClientsModule } from './clients/clients.module';
+import { OrdersModule } from './orders/orders.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ClientsModule,
+    LoginModule,
+    OrdersModule,
+    PageNotFoundModule,
+    SharedModule,
+    UiModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
